91393b2d26373191569e225cea6c920ac688ce74
Алексей Сенотрусов <saper639@gmail.com>
2019-10-11 16:40:59 +0800

    Example 4

--- example_3/ui.js
+++ example_3/ui.js
@@ -1,30 +1,36 @@
 COMPONENT('imgview', function(self, config) {
 	var select, preview, content = null;
 	var render = '';
+	//конфигурирование компоненента при его инициализации
 	self.configure = function(key, value, init) {
 		if (init) return;
 		if (key=='datasource') self.datasource(value, self.bind);		
 	};	
 	self.redraw = function() {		
-		var html = '<select data-jc-bind="" size="5" style="width:150px;">{0}</select><div class="preview"></div>'.format(render);		
+		var html = '<select data-jc-bind="" size="5" style="width:150px;">{0}</select><span class="preview"></span>'.format(render);		
 		self.html(html);
 		select = self.find('select');
 		preview = self.find('.preview');
 		render && self.refresh();			
 	};	
+	//наблюдает за изменениями в модели в соответствии с опред. path, при выборе в списке меняем аватарку
 	self.setter = function(value) {
 		if (!value) return;
 		var img = self.preview.format(value);
 		preview.html(img);
+		select.find('option').each(function(el) {
+ 		    var el = $(this);
+                    var is = el.attr('value') === value;
+                    el.attr('selected', is);  
+                })
 	};	
+	//привязываем datasource к нашему списку
 	self.bind = function(path, arr) {		
-		if (!arr)
-			arr = EMPTYARRAY;		
+		if (!arr) arr = EMPTYARRAY;		
 		var builder = [];
 		var value = self.get();
 		var propText = config.text || 'name';
 		var propValue = config.value || 'url';
-		config.empty !== undefined && builder.push('<option value="">{0}</option>'.format(config.empty));
 		for (var i = 0, length = arr.length; i < length; i++) {
 			var item = arr[i];			
 			builder.push(self.template({ value: item[propValue], selected: value == item[propValue], text: item[propText] }));
@@ -32,6 +38,7 @@ COMPONENT('imgview', function(self, config) {
 		render = builder.join('');
 		select.html(render);
 	};	
+	//выполняется при создании компонента
 	self.make = function() {		
 		self.template = Tangular.compile('<option value="{{value}}"{{if selected}} selected="selected"{{ fi }}>{{text}}</option>');
 		self.preview = '<img src="{0}" style="width:100px">';
---
+++ example_4/ui.js
@@ -0,0 +1,390 @@
+/* REPEATER */
+COMPONENT('repeater', 'hidden:true;check:true', function(self, config) {
+
+	var filter = null;
+	var recompile = false;
+	var reg = /\$(index|path)/g;
+
+	self.readonly();
+
+	self.configure = function(key, value) {
+		if (key === 'filter')
+			filter = value ? GET(value) : null;
+	};
+
+	self.make = function() {
+		var element = self.find('script');
+
+		if (!element.length) {
+			element = self.element;
+			self.element = self.element.parent();
+		}
+
+		var html = element.html();
+		element.remove();
+		self.template = Tangular.compile(html);
+		recompile = (/data-jc="|data-bind="/).test(html);
+	};
+
+	self.setter = function(value) {
+
+		if (!value || !value.length) {
+			config.hidden && self.aclass('hidden');
+			self.empty();
+			self.cache = '';
+			return;
+		}
+
+		var builder = [];
+		for (var i = 0, length = value.length; i < length; i++) {
+			var item = value[i];
+			item.index = i;
+			if (!filter || filter(item)) {
+				builder.push(self.template(item).replace(reg, function(text) {
+					return text.substring(0, 2) === '$i' ? i.toString() : self.path + '[' + i + ']';
+				}));
+			}
+		}
+
+		var tmp = builder.join('');
+
+		if (config.check) {
+			if (tmp === self.cache)
+				return;
+			self.cache = tmp;
+		}
+
+		self.html(tmp);
+		config.hidden && self.rclass('hidden');
+		recompile && self.compile();
+	};
+});
+/* REPEATER */
+/*textbox*/
+COMPONENT('textbox', function(self, config) {
+
+	var input, content = null, isfilled = false;
+	var innerlabel = function() {
+		var is = !!input[0].value;
+		if (isfilled !== is) {
+			isfilled = is;
+			self.tclass('ui-textbox-filled', isfilled);
+		}
+	};
+
+	self.nocompile && self.nocompile();
+
+	self.validate = function(value) {
+
+		if ((!config.required || config.disabled) && !self.forcedvalidation())
+			return true;
+
+		if (self.type === 'date')
+			return value instanceof Date && !isNaN(value.getTime());
+
+		if (value == null)
+			value = '';
+		else
+			value = value.toString();
+
+		EMIT('reflow', self.name);
+
+		if (config.minlength && value.length < config.minlength)
+			return false;
+
+		switch (self.type) {
+			case 'email':
+				return value.isEmail();
+			case 'phone':
+				return value.isPhone();
+			case 'url':
+				return value.isURL();
+			case 'currency':
+			case 'number':
+				return value > 0;
+		}
+
+		return config.validation ? !!self.evaluate(value, config.validation, true) : value.length > 0;
+	};
+
+	self.make = function() {
+
+		content = self.html();
+
+		self.type = config.type;
+		self.format = config.format;
+
+		self.event('click', '.fa-calendar', function(e) {
+			if (!config.disabled && !config.readonly && config.type === 'date') {
+				e.preventDefault();
+				SETTER('calendar', 'toggle', self.element, self.get(), function(date) {
+					self.change(true);
+					self.set(date);
+				});
+			}
+		});
+
+		self.event('click', '.fa-caret-up,.fa-caret-down', function() {
+			if (!config.disabled && !config.readonly && config.increment) {
+				var el = $(this);
+				var inc = el.hclass('fa-caret-up') ? 1 : -1;
+				self.change(true);
+				self.inc(inc);
+			}
+		});
+
+		self.event('click', '.ui-textbox-label', function() {
+			input.focus();
+		});
+
+		self.event('click', '.ui-textbox-control-icon', function() {
+			if (config.disabled || config.readonly)
+				return;
+			if (self.type === 'search') {
+				self.$stateremoved = false;
+				$(this).rclass('fa-times').aclass('fa-search');
+				self.set('');
+			} else if (self.type === 'password') {
+				var el = $(this);
+				var type = input.attr('type');
+
+				input.attr('type', type === 'text' ? 'password' : 'text');
+				el.rclass2('fa-').aclass(type === 'text' ? 'fa-eye' : 'fa-eye-slash');
+			} else if (config.iconclick)
+				EXEC(config.iconclick, self);
+		});
+
+		self.event('focus', 'input', function() {
+			if (!config.disabled && !config.readonly && config.autocomplete)
+				EXEC(config.autocomplete, self);
+		});
+
+		self.event('input', 'input', innerlabel);
+		self.redraw();
+		config.iconclick && self.configure('iconclick', config.iconclick);
+	};
+
+	self.setter2 = function(value) {
+		if (self.type === 'search') {
+			if (self.$stateremoved && !value)
+				return;
+			self.$stateremoved = !value;
+			self.find('.ui-textbox-control-icon').tclass('fa-times', !!value).tclass('fa-search', !value);
+		}
+		innerlabel();
+	};
+
+	self.redraw = function() {
+
+		var attrs = [];
+		var builder = [];
+		var tmp = 'text';
+
+		switch (config.type) {
+			case 'password':
+				tmp = config.type;
+				break;
+			case 'number':
+			case 'phone':
+				isMOBILE && (tmp = 'tel');
+				break;
+		}
+
+		self.tclass('ui-disabled', !!config.disabled);
+		self.tclass('ui-textbox-required', !!config.required);
+		self.type = config.type;
+		attrs.attr('type', tmp);
+		config.placeholder && !config.innerlabel && attrs.attr('placeholder', config.placeholder);
+		config.maxlength && attrs.attr('maxlength', config.maxlength);
+		config.keypress != null && attrs.attr('data-jc-keypress', config.keypress);
+		config.delay && attrs.attr('data-jc-keypress-delay', config.delay);
+		config.disabled && attrs.attr('disabled');
+		config.readonly && attrs.attr('readonly');
+		config.error && attrs.attr('error');
+	        config.class && attrs.attr('class', config.class);
+        	config.id && attrs.attr('id', config.id);
+
+		attrs.attr('data-jc-bind', '');
+
+		if (config.autofill) {
+			attrs.attr('name', self.path.replace(/\./g, '_'));
+			self.autofill && self.autofill();
+		} else {
+			attrs.attr('name', 'input' + Date.now());
+			attrs.attr('autocomplete', 'new-password');
+		}
+
+		config.align && attrs.attr('class', 'ui-' + config.align);
+		!isMOBILE && config.autofocus && attrs.attr('autofocus');
+
+		builder.push('<div class="ui-textbox-input"><input {0} /></div>'.format(attrs.join(' ')));
+
+		var icon = config.icon;
+		var icon2 = config.icon2;
+
+		if (!icon2 && self.type === 'date')
+			icon2 = 'calendar';
+		else if (!icon2 && self.type === 'password')
+			icon2 = 'eye';
+		else if (self.type === 'search')
+			icon2 = 'search';
+
+		icon2 && builder.push('<div class="ui-textbox-control"><span class="fa fa-{0} ui-textbox-control-icon"></span></div>'.format(icon2));
+		config.increment && !icon2 && builder.push('<div class="ui-textbox-control"><span class="fa fa-caret-up"></span><span class="fa fa-caret-down"></span></div>');
+
+		if (config.label)
+			content = config.label;
+
+		self.tclass('ui-textbox-innerlabel', !!config.innerlabel);
+
+		if (content.length) {
+			var html = builder.join('');
+			builder = [];
+			builder.push('<div class="ui-textbox-label">');
+			icon && builder.push('<i class="fa fa-{0}"></i> '.format(icon));
+			builder.push('<span>' + content + (content.substring(content.length - 1) === '?' ? '' : ':') + '</span>');
+			builder.push('</div><div class="ui-textbox">{0}</div>'.format(html));
+			config.error && builder.push('<div class="ui-textbox-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
+			self.html(builder.join(''));
+			self.aclass('ui-textbox-container');
+			input = self.find('input');
+		} else {
+			config.error && builder.push('<div class="ui-textbox-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
+			self.aclass('ui-textbox ui-textbox-container');
+			self.html(builder.join(''));
+			input = self.find('input');
+		}
+                if (config.class) { 
+			self.find('.ui-textbox-input').rclass('ui-textbox-input');
+                        self.find('.ui-textbox').rclass('ui-textbox'); 
+			self.rclass('ui-textbox'); 
+                } 
+	};
+
+	self.configure = function(key, value, init) {
+
+		if (init)
+			return;
+
+		var redraw = false;
+
+		switch (key) {
+			case 'readonly':
+				self.find('input').prop('readonly', value);
+				break;
+			case 'disabled':
+				self.tclass('ui-disabled', value);
+				self.find('input').prop('disabled', value);
+				self.reset();
+				break;
+			case 'format':
+				self.format = value;
+				self.refresh();
+				break;
+			case 'required':
+				self.noValid(!value);
+				!value && self.state(1, 1);
+				self.tclass('ui-textbox-required', value === true);
+				break;
+			case 'placeholder':
+				input.prop('placeholder', value || '');
+				break;
+			case 'maxlength':
+				input.prop('maxlength', value || 1000);
+				break;
+			case 'autofill':
+				input.prop('name', value ? self.path.replace(/\./g, '_') : '');
+				break;
+			case 'label':
+				if (content && value)
+					self.find('.ui-textbox-label span').html(value);
+				else
+					redraw = true;
+				content = value;
+				break;
+			case 'type':
+				self.type = value;
+				if (value === 'password')
+					value = 'password';
+				else
+					self.type = 'text';
+				self.find('input').prop('type', self.type);
+				break;
+			case 'align':
+				input.rclass(input.attr('class')).aclass('ui-' + value || 'left');
+				break;
+			case 'autofocus':
+				input.focus();
+				break;
+			case 'icon2click': // backward compatibility
+			case 'iconclick':
+				config.iconclick = value;
+				self.find('.ui-textbox-control').css('cursor', value ? 'pointer' : 'default');
+				break;
+			case 'icon':
+				var tmp = self.find('.ui-textbox-label .fa');
+				if (tmp.length)
+					tmp.rclass2('fa-').aclass('fa-' + value);
+				else
+					redraw = true;
+				break;
+			case 'icon2':
+			case 'increment':
+				redraw = true;
+				break;
+			case 'labeltype':
+				redraw = true;
+				break;
+		}
+
+		redraw && setTimeout2('redraw.' + self.id, function() {
+			self.redraw();
+			self.refresh();
+		}, 100);
+	};
+
+	self.formatter(function(path, value) {
+		if (value) {
+			switch (config.type) {
+				case 'lower':
+					value = value.toString().toLowerCase();
+					break;
+				case 'upper':
+					value = value.toString().toUpperCase();
+					break;
+			}
+		}
+		return config.type === 'date' ? (value ? value.format(config.format || 'yyyy-MM-dd') : value) : value;
+	});
+
+	self.parser(function(path, value) {
+		if (value) {
+			switch (config.type) {
+				case 'lower':
+					value = value.toLowerCase();
+					break;
+				case 'upper':
+					value = value.toUpperCase();
+					break;
+			}
+		}
+		return value ? config.spaces === false ? value.replace(/\s/g, '') : value : value;
+	});
+
+	self.state = function(type) {
+		if (!type)
+			return;
+		var invalid = config.required ? self.isInvalid() : self.forcedvalidation() ? self.isInvalid() : false;
+		if (invalid === self.$oldstate)
+			return;
+		self.$oldstate = invalid;
+		self.tclass('ui-textbox-invalid', invalid);
+		config.error && self.find('.ui-textbox-helper').tclass('ui-textbox-helper-show', invalid);
+	};
+
+	self.forcedvalidation = function() {
+		var val = self.get();
+		return (self.type === 'phone' || self.type === 'email') && (val != null && (typeof val === 'string' && val.length !== 0));
+	};
+});
+/*textbox*/
