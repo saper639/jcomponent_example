9dec0528b40b26791dd6299e9b4f0ec5ec79a965
Алексей Сенотрусов <saper639@gmail.com>
2019-11-29 18:05:56 +0800

    update

--- example_6/spa_with_totaljs/public/js/app.js
+++ example_6/spa_with_totaljs/public/js/app.js
@@ -8,3 +8,25 @@
   var arr_sortby = [{id: 'relevancy', name: 'Relevancy'}, {id: 'popularity', name: 'Popularity'}, {id: 'publishedAt', name: 'PublishedAt'} ];
   var arr_language = [{id: 'de', name: 'Deutsch'}, {id: 'en', name: 'English'}, {id: 'es', name: 'Español'}, {id: 'fr', name: 'Français'},
 		      {id: 'it', name: 'Italiano'}, {id: 'ru', name: 'Русский'}, {id: 'zh', name: '中文'} ];
+  var options_default = {country: 'ru', category:'general', language: 'ru', sortBy: 'publishedAt'};
+
+  SET('option', CACHE('option')||options_default);
+
+  NAV.clientside('.R');	
+
+  NAV.route('/search', ()=>{
+    SET('common.page', 'search');
+  });
+
+  NAV.route('/top', ()=>{
+    SET('common.page', 'top');
+  });
+
+  NAV.route('/history', ()=>{
+    SET('common.page', 'history');
+  });
+
+  NAV.route('/setting', ()=>{
+    SET('common.page', 'setting');
+  });
+
