ab9d4f61a4e8b6086e569e58b2d0157447ef7324
saper639 <saper639@gmail.com>
2019-12-02 02:30:42 +0800

    update

--- example_6/spa_with_totaljs/controllers/defaul.js
+++ example_6/spa_with_totaljs/controllers/defaul.js
@@ -1,3 +1,15 @@
 exports.install = function() {
-	ROUTE('/*', 'index');
+	ROUTE('/*', 			  'index');
+	ROUTE('/api/news/search', news_search);
+	ROUTE('/api/news/top', 	  news_top);
 };
+
+function news_search() {
+	var self = this;
+	self.plain('search');
+}
+
+function news_top() {
+	var self = this;		
+	self.plain('top');
+}
